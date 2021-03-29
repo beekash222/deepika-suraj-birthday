58efa61098ad76084b42089bb4b2c3e0b750ba37
Beekash Mohanty <39214665+beekash222@users.noreply.github.com>
2021-03-29 09:47:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -21,28 +21,24 @@ def predict():
 @app.route('/predict1',methods=['GET','POST'])
 def predict1():
     select = request.form.get("cars","None")
-    if select == "Deepu":
-        x ="Happy birthday Rabbit, hope you liked it I know ur love for coding,hence thought of doing thissome people are meant to be in ur life and u r the one for me thnq for being there..love you laods,,,enjoy ur day  keeping smiling , loving ,enjoying and always be happy may god fulfils ur all dreams and accept your prayer nd grant ur all wishes looking forward for a bright and happy future together thnq for not ruining my surprise this time ..loads of love for that P.S. I love you" 
-    elif select == "Neha":
-        x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"
-    elif select == "Mahak":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"
-    elif select == "Swati":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Vinisha":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Shivam":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Rohan":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Anwesha":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Manjeet":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Priyabart":
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
-    elif select == "Debu":    
-       x = "Happy Birthday Suraj,May God bless you!!""\n""Jaldi se tere aur Deepu ki shaddi ho i want to see fusion marriage(odia+haryanvi)""\n""Bhagwan tjhe bahut sare bache de ..aur unki parwarish k lia bahut sare paise bhi de"        
+    if select == "Beekash":
+        x =''''Hello Lopy,
+
+Its been 5 days with you and i know there is no such thing as a concidence! people met for a reason but whatever it is , i am so happy and glad that i have you in my life and whatever happenes in future, i will always with you becuase i know i will never have another friend like you.
+
+Thanks for tolerating me and my crazy habits i know its not easy but trust me i never do anything intentionally to hurt you.
+
+Your love for your friends is an exquisite and sometimes i feel jealous that i never had any friendship like this with anyone.
+
+   Your presence makes me feel alive
+   You are funny and witty 
+   I feel more connected and comfortable with you 
+   You always make me smile by your crazy jokes and incidents this make my sadness disappear.
+
+Someone well said "An empty stomach need food, an empty brain needs knowledge, an empty house needs a family, and an empty heart needs love. But then, an empty life needs a friend, Thanks for filing in"
+
+
+Thanks for being wih me !'''        
     return x
 
 @app.route('/predict2',methods=['GET','POST'])
@@ -53,10 +49,10 @@ def predict2():
 @app.route('/predict3',methods=['GET','POST'])
 def predict3():
     if 'Scratch_Me1' in request.form:
-        x = "You won lots of kissess & hugs and free shopping..."
+        x = "You won free shopping with me"
         return x
     if 'Scratch_Me2' in request.form:
-        x = "You won dinner date..."
+        x = "You won a dinner date with me"
         return x
     if 'Scratch_Me3' in request.form:
         x = "You won a trip with me"
